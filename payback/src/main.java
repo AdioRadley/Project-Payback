62f34f65f9b624109a75d10bf20b5ba295883e4e
Sam Mulligan <Cherehroo@gmail.com>
2012-05-19 13:15:24 -0500

    commit practice 2

--- payback/src/main.java
+++ payback/src/main.java
@@ -1,11 +1,15 @@
-
+import java.io.*;
 public class main {
 
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 String s = br.readLine();
+		 System.out.println(s);
 
 	}
 
